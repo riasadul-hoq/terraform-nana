aeb4974d9634a588eeb91adefdb2831525c468e1
Riasadul Hoq <shaon777@gmail.com>
2022-01-24 13:47:47 +0000

    Sensitive info has been removed & Variables added

--- main.tf
+++ main.tf
@@ -1,6 +1,26 @@
 provider "aws" {
   region = "eu-west-3"
-  access_key = "AKIARO4JGNECY5DUPD5A"
-  secret_key = "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"
+  profile = "contino_sandbox"
 }
 
+
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "env_prefix" {}
+variable "avail_zone" {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id 
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = { 
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
