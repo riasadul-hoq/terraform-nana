133c852035839125709403f2c896895d48023058
Riasadul Hoq <shaon777@gmail.com>
2022-01-26 14:08:33 +0000

    Bash script has been added

---
+++ startup-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Bash Script to install & run docker image 
+
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+#sudo chmod 666 /var/run/docker.sock
+docker run -p 8080:80 nginx
